a040f92d727c52f14048c7e430d52a1d3b115a77
Angelo Flores <angeloflores88@gmail.com>
2020-11-17 15:16:34 -0800

    Router and Experience component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
+import {Link, Route, Switch} from 'react-router-dom';
 import './App.css';
+import Experience from './pages/Experience'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Angelo Flores test
-        </p>
+        <Link to={'/experience'}>Experience</Link>
       </header>
+
+        <Switch>
+            <Route exact path={'/experience'} component={Experience}/>
+        </Switch>
     </div>
   );
 }
