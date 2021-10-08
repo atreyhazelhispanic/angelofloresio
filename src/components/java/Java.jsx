29a7a76274f6abeadf527a4858616381a81be9e3
Angelo Flores <angeloflores88@gmail.com>
2021-10-08 14:46:09 -0700

    removing projects

--- src/components/java/Java.jsx
+++ src/components/java/Java.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import OisIndexer from "./OisIndexer";
-import CuratedCollections from "./CuratedCollections";
 
 class Java extends React.Component {
     render() {
         return (
             <div>
-                <OisIndexer/>
-                <CuratedCollections/>
+                Place work here.
             </div>
         );
     }
